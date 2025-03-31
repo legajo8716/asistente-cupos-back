a3019879f1bd99324cfb5b934ae8ef2319e42358
nelsontechforb <nelson@techforb.com>
2025-03-31 19:59:26 -0300

    agrego ResponseEntity al servicio consulta

--- src/main/java/com/edu/asistenteCupos/controller/AlumnoController.java
+++ src/main/java/com/edu/asistenteCupos/controller/AlumnoController.java
@@ -4,7 +4,12 @@ import com.edu.asistenteCupos.domain.Alumno;
 import com.edu.asistenteCupos.service.AlumnoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
@@ -17,19 +22,25 @@ class AlumnoController {
     AlumnoService alumnoService;
 
 
-
-
     @GetMapping
 
-    public List<Alumno> getAlumnos(){
+    public List<Alumno> getAlumnos() {
 
         return alumnoService.getAlumnos();
     }
+
     @GetMapping("/consultar")
-    public String consultar(@RequestParam String userInput){
-      return  alumnoService.consultar(userInput);
+    public ResponseEntity<String> consultar(@RequestParam String userInput) {
+        try {
+           // String respuesta = alumnoService.consultar(userInput);
+            String respuesta = "La respuesta es: " + userInput ;
+            ResponseEntity<String> response = ResponseEntity.ok(respuesta);
+            System.out.println("Respuesta: " + response);
 
-
+            return response; // Devuelve una respuesta 200 con el resultado
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error en la consulta: " + e.getMessage());
+        }
     }
-
 }
