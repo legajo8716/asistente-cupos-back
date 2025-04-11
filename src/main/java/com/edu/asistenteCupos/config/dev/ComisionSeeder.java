545c2b9707570d7638634a245dc7363050613b3f
Marcos Alvarenga <alvarenga.marcos.daniel@gmail.com>
2025-04-11 03:14:34 -0300

    Inicializar Beans en orden por dependencia

--- src/main/java/com/edu/asistenteCupos/config/dev/ComisionSeeder.java
+++ src/main/java/com/edu/asistenteCupos/config/dev/ComisionSeeder.java
@@ -10,6 +10,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 
 import java.util.List;
 import java.util.Optional;
@@ -48,6 +49,7 @@ public class ComisionSeeder {
   }
 
   @Bean
+  @Order(2)
   @Profile("dev")
   CommandLineRunner runComisionSeeder() {
     return args -> cargarComisiones(nombreCsv);
--- src/main/java/com/edu/asistenteCupos/config/dev/MateriasSeeder.java
+++ src/main/java/com/edu/asistenteCupos/config/dev/MateriasSeeder.java
@@ -8,6 +8,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
 
 import java.util.*;
 
@@ -25,6 +26,7 @@ public class MateriasSeeder {
   }
 
   @Bean
+  @Order(1)
   @Profile("dev")
   CommandLineRunner runMateriasSeeder() {
     return args -> cargarMaterias(nombreCsv);
